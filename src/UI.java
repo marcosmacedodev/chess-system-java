9b5859b62c8d9b06c01b4edf1a9e0c573d01e59a
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-08-17 15:38:50 -0300

    Implementado metodo readChessPosition

--- src/UI.java
+++ src/UI.java
@@ -1,4 +1,8 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chesslayer.ChessPiece;
+import chesslayer.ChessPosition;
 
 public class UI {
 	
@@ -14,7 +18,8 @@ public class UI {
 	}
 	
 	private static void printPiece(ChessPiece piece) {
-		if(piece == null) {
+		if(piece == null) 
+		{
 			System.out.print("-");
 		}
 		else
@@ -22,6 +27,20 @@ public class UI {
 			System.out.print(piece);
 		}
 		System.out.print(" ");
+	}
+	
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try 
+		{
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(row, column);
+		}
+		catch(RuntimeException ex) 
+		{
+			throw new InputMismatchException("Erro reading ChessPosition. Valid values are from a1 to h8.");
+		}
 	}
 
 }
