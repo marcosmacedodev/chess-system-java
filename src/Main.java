77d4e41cce62102c5e1d57735e9badec1c27bb2d
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-08-17 16:17:58 -0300

    Loop

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 import chesslayer.ChessMatch;
+import chesslayer.ChessPiece;
+import chesslayer.ChessPosition;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch cm = new ChessMatch();
-		UI.printBoard(cm.getPieces());
+		
+		while(true) {
+			UI.printBoard(cm.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = cm.performChessMove(source, target);
+		}
+		
 
 	}
 
