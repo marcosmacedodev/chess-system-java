fde62cc82e204154b0449fee02309514ed2c428e
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-08-18 15:14:28 -0300

    Metodo canMove, implementado movimento King

--- src/chesslayer/pieces/King.java
+++ src/chesslayer/pieces/King.java
@@ -1,6 +1,7 @@
 package chesslayer.pieces;
 
 import boardlayer.Board;
+import boardlayer.Position;
 import chesslayer.ChessPiece;
 import chesslayer.Color;
 
@@ -21,7 +22,70 @@ public class King extends ChessPiece {
 	public boolean[][] possibleMoves() {
 		// TODO Auto-generated method stub
 		boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		//Above
+		Position p = new Position(0, 0);
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if(getBoard().positionExists(p) && canMove(p))
+		{
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Below
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if(getBoard().positionExists(p) && canMove(p))
+		{
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Left
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p))
+		{
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p))
+		{
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Nw
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p))
+		{
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Ne
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p))
+		{
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Sw
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p))
+		{
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Se
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p))
+		{
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
 		return possibleMoves;
+	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece)getBoard().piece(position);
+		return piece == null || piece.getColor() != getColor();
 	}
 
 }
