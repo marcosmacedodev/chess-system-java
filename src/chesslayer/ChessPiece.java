423d0280d9949a08968e051471d2d123630055a9
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-08-19 15:09:56 -0300

    Contagem de movimentos pecas

--- src/chesslayer/ChessPiece.java
+++ src/chesslayer/ChessPiece.java
@@ -7,22 +7,44 @@ import boardlayer.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
-	public ChessPiece(Board board, Color color) {
+	private int moveCount;
+	
+	public ChessPiece(Board board, Color color) 
+	{
 		super(board);
 		// TODO Auto-generated constructor stub
 		this.color = color;
 	}
 
-	public Color getColor() {
+	public Color getColor() 
+	{
 		return color;
 	}
 	
-	protected boolean isThereOpponentPiece(Position position) {
+	public int getMoveCount()
+	{
+		return moveCount;
+	}
+	
+	
+	public void increaseMoveCount() 
+	{
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() 
+	{
+		moveCount--;
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) 
+	{
 		ChessPiece piece = (ChessPiece)getBoard().piece(position);
 		return piece != null && piece.getColor() != color;
 	}
 	
-	public ChessPosition getChessPosition() {
+	public ChessPosition getChessPosition() 
+	{
 		return ChessPosition.fromPosition(position);
 	}
 
